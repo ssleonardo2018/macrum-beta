66b27192c5f2c2ebb8259f445302d288410a4dd3
ssleonardo2018 <115373834+ssleonardo2018@users.noreply.github.com>
2026-04-19 03:59:16 -0300

    Add service worker registration for PWA support

--- static/js/nutricionista.js
+++ static/js/nutricionista.js
@@ -12,3 +12,12 @@ function logout() {
     // redireciona para login
     window.location.href = "index.html";
 }
+
+<script>
+  if ('serviceWorker' in navigator) {
+    // O escopo garante que o SW não toque na index.html da raiz
+    navigator.serviceWorker.register('/templates/sw.js', { scope: '/templates/' })
+      .then(reg => console.log('PWA ativo apenas em /templates/'))
+      .catch(err => console.error('Erro de registro:', err));
+  }
+</script>
